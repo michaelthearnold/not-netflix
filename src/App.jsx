99d03e3acf08bbb4788962481e1c181dacb21586
Michael Arnold <michaelthearnold@gmail.com>
2017-08-24 14:16:51 -0700

    Added add/remove functionality to movie lists

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import MovieData from './MovieData'
 import Movie from './Movie'
 
+
 class App extends Component {
   getInitialState() {
     return {
@@ -12,16 +13,19 @@ class App extends Component {
     }
   }
 
+
   constructor(props) {
     super(props)
     //initialize state
     this.state = this.getInitialState()
   }
 
+
   componentDidMount() {
     //get movie data from server
     this.updateMovies()
   }
+
 
   updateMovies() {
     //normally we would do an AJAX call to the backend to get some actual data here.
@@ -32,22 +36,76 @@ class App extends Component {
     })
   }
 
+
+  moveBetweenLists(movie, sourceProp, destProp) {
+    //duplicate current state to prevent tampering with it
+    var source = this.state[sourceProp].slice()
+    var dest = this.state[destProp].slice()
+
+    //get index of movie being removed
+    var index = source.indexOf(movie)
+
+    //swap it from one list to the other
+    dest.push(source[index])
+    source.splice(index, 1)
+
+    //create new state to be updated, and map it to local state copies
+    var state = {}
+    state[sourceProp] = source
+    state[destProp] = dest
+    this.setState(state)
+  }
+
+
   getMyListMovies() {
     var movies = this.state.myList.map(movie => {
       return (
         <Movie title={movie.title}
           img={movie.img}
           key={movie.id}>
-          <button>Remove</button>
+
+          {/* child to be displayed in popup on hover */}
+          <Bs.Button bsStyle={"danger"} 
+            onClick={this.moveBetweenLists.bind(this, movie, "myList", "recommendations")}>
+
+            Remove
+          </Bs.Button>
         </Movie>
       )
     })
+
     return (
       <Bs.Row>
         {movies}
       </Bs.Row>
     )
   }
+
+
+  getRecommendedMovies() {
+    var movies = this.state.recommendations.map(movie => {
+      return (
+        <Movie title={movie.title}
+          img={movie.img}
+          key={movie.id}>
+
+          {/* child to be displayed in popup on hover */}
+          <Bs.Button bsStyle={"success"}
+            onClick={this.moveBetweenLists.bind(this, movie, "recommendations", "myList")}>
+
+            Add
+          </Bs.Button>
+        </Movie>
+      )
+    })
+
+    return (
+      <Bs.Row>
+        {movies}
+      </Bs.Row>
+    )
+  }
+
 
   getStyle() {
     return {
@@ -56,6 +114,7 @@ class App extends Component {
       }
     }
   }
+
 
   render() {
     var style = this.getStyle()
@@ -69,6 +128,7 @@ class App extends Component {
         {this.getMyListMovies()}
 
         {/* Recommendation Movies */}
+        {this.getRecommendedMovies()}
 
         {/* MyList text */}
 
@@ -76,5 +136,6 @@ class App extends Component {
     )
   }
 }
+
 
 export default App
