83446d9737928e80b10703a779ffa8f0fb9b3427
Michael Arnold <michaelthearnold@gmail.com>
2017-08-24 13:25:42 -0700

    Added movie componant

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import * as Bs from 'react-bootstrap';
+import React, { Component } from 'react'
+import * as Bs from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import MovieData from './MovieData'
+import Movie from './Movie'
 
 class App extends Component {
-
   getInitialState() {
     return {
       recommendations: [],
-      favorites: []
+      myList: []
     }
   }
 
@@ -15,12 +16,37 @@ class App extends Component {
     super(props)
     //initialize state
     this.state = this.getInitialState()
+  }
+
+  componentDidMount() {
     //get movie data from server
     this.updateMovies()
   }
 
   updateMovies() {
-    
+    //normally we would do an AJAX call to the backend to get some actual data here.
+    //Instead, we'll give some dummy data imported from a local file
+    this.setState({
+      recommendations: MovieData.recommendations,
+      myList: MovieData.myList
+    })
+  }
+
+  getMyListMovies() {
+    var movies = this.state.myList.map(movie => {
+      return (
+        <Movie title={movie.title}
+          img={movie.img}
+          key={movie.id}>
+          <button>Remove</button>
+        </Movie>
+      )
+    })
+    return (
+      <Bs.Row>
+        {movies}
+      </Bs.Row>
+    )
   }
 
   getStyle() {
@@ -35,11 +61,20 @@ class App extends Component {
     var style = this.getStyle()
     return (
       <div>
+        {/* Header */}
         <h1 style={style.center}>Not Netflix</h1>
         <h3 style={style.center}>All 6 of your favorite movies in one place.</h3>
+
+        {/* MyList Movies */}
+        {this.getMyListMovies()}
+
+        {/* Recommendation Movies */}
+
+        {/* MyList text */}
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
